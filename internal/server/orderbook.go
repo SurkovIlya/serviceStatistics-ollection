4c2fadc7d99b5b98f8e6f9283698b2ceff4eae0a
Ilya-SurkoV <lsurkovl@mail.ru>
2024-07-12 16:05:10 +0300

    add: handlers

---
+++ internal/server/orderbook.go
@@ -0,0 +1,11 @@
+package server
+
+import "net/http"
+
+func GetOrderBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func SaveOrderBook(w http.ResponseWriter, r *http.Request) {
+
+}
---
+++ internal/server/orderhistory.go
@@ -0,0 +1,13 @@
+package server
+
+import (
+	"net/http"
+)
+
+func GetOrderHistory(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func SaveOrderHistory(w http.ResponseWriter, r *http.Request) {
+
+}
