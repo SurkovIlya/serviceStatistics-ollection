3567f2b2799d4199d6b3396e1c2602e536a90ee9
Ilya-SurkoV <lsurkovl@mail.ru>
2024-07-12 16:02:45 +0300

    add: postgres pkg

---
+++ pkg/postgres/postgres.go
@@ -0,0 +1,39 @@
+package pg
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+var ErrNoMatch = fmt.Errorf("no matching record")
+
+type Database struct {
+	Conn *sql.DB
+}
+
+type DBParams struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+	Database string
+}
+
+func Initialize(params DBParams) (Database, error) {
+	db := Database{}
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		params.Host, params.Port, params.Username, params.Password, params.Database)
+	conn, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return db, err
+	}
+
+	db.Conn = conn
+	err = db.Conn.Ping()
+	if err != nil {
+		return db, err
+	}
+	log.Println("Database connection established")
+	return db, nil
+}
