a1b81ff9d9eac39e6573830e9491cb926b1d437e
Ilya-SurkoV <lsurkovl@mail.ru>
2024-07-12 16:06:07 +0300

    test: main

--- main.go
+++ main.go
@@ -1,4 +1,33 @@
 package main
 
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/SurkovIlya/statistics-app/internal/server"
+)
+
 func main() {
+	srv := server.New("8080")
+
+	go func() {
+		if err := srv.Run(); err != nil {
+			log.Fatalf("error occured while running http server: %s", err.Error())
+		}
+	}()
+
+	log.Print("app Started")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	log.Print("app Shutting Down")
+
+	if err := srv.Shutdown(context.Background()); err != nil {
+		log.Panicf("error occured on server shutting down: %s", err.Error())
+	}
 }
