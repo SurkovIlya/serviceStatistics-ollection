e95a83c87b0c6a16703aba84b8cdf157a8b4b834
Ilya-SurkoV <lsurkovl@mail.ru>
2024-07-13 16:30:38 +0300

    add: postgres funcs

--- internal/orders/order.go
+++ internal/orders/order.go
@@ -1,29 +1,57 @@
 package orders
 
 import (
+	"fmt"
+
 	"github.com/SurkovIlya/statistics-app/internal/model"
-	"github.com/SurkovIlya/statistics-app/pkg/postgres"
 )
 
-type OrderManager struct {
-	storage *postgres.Database
+type PostgresStorage interface {
+	SelectOrder(exchange_name, pair string) ([]*model.DepthOrder, error)
+	InsertOrderBook(exchange_name, pair string, orderBook []*model.DepthOrder) error
+	SelectHistory(client *model.Client) ([]*model.HistoryOrder, error)
+	InsertHistory(client *model.Client, order *model.HistoryOrder) error
 }
 
-func New(storage *postgres.Database) *OrderManager {
+type OrderManager struct {
+	QueryStorage PostgresStorage
+}
+
+func New(query PostgresStorage) *OrderManager {
 	return &OrderManager{
-		storage: storage,
+		QueryStorage: query,
 	}
 }
 
 func (om *OrderManager) GetOrderBook(exchange_name, pair string) ([]*model.DepthOrder, error) {
-	return nil, nil
+	res, err := om.QueryStorage.SelectOrder(exchange_name, pair)
+	if err != nil {
+		return nil, fmt.Errorf("error get order from BD: %s", err)
+	}
+
+	return res, nil
 }
 func (om *OrderManager) SaveOrderBook(exchange_name, pair string, orderBook []*model.DepthOrder) error {
+	err := om.QueryStorage.InsertOrderBook(exchange_name, pair, orderBook)
+	if err != nil {
+		return fmt.Errorf("error save order in BD: %s", err)
+	}
+
 	return nil
 }
 func (om *OrderManager) GetOrderHistory(client *model.Client) ([]*model.HistoryOrder, error) {
-	return nil, nil
+	res, err := om.QueryStorage.SelectHistory(client)
+	if err != nil {
+		return nil, fmt.Errorf("error get history from BD: %s", err)
+	}
+
+	return res, nil
 }
 func (om *OrderManager) SaveOrder(client *model.Client, order *model.HistoryOrder) error {
+	err := om.QueryStorage.InsertHistory(client, order)
+	if err != nil {
+		return fmt.Errorf("error save history in BD: %s", err)
+	}
+
 	return nil
 }
--- internal/storage/pg/pg.go
+++ internal/storage/pg/pg.go
@@ -1,1 +1,132 @@
 package pg
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/SurkovIlya/statistics-app/internal/model"
+	"github.com/SurkovIlya/statistics-app/pkg/postgres"
+)
+
+const maxOrderScan = 1000
+
+type PostgresStorage struct {
+	storage *postgres.Database
+}
+
+func New(storage *postgres.Database) *PostgresStorage {
+	return &PostgresStorage{
+		storage: storage,
+	}
+}
+
+func (psq *PostgresStorage) SelectOrder(exchange_name, pair string) ([]*model.DepthOrder, error) {
+	query := `SELECT asks, bids FROM order_book WHERE exchange = $1 AND pair = $2`
+	rows, err := psq.storage.Conn.Query(query, exchange_name, pair)
+	if err != nil {
+		return nil, fmt.Errorf("error query: %s", err)
+	}
+	defer rows.Close()
+
+	res := make([]*model.DepthOrder, 0, maxOrderScan)
+
+	for rows.Next() {
+		var asksJSON interface{}
+		var bidsJSON interface{}
+		var asks *model.DepthOrder
+		var bids *model.DepthOrder
+
+		err := rows.Scan(&asksJSON, &bidsJSON)
+		if err != nil {
+			log.Printf("error scan rows: %s", err)
+		}
+
+		err = json.Unmarshal(asksJSON.([]byte), &asks)
+		if err != nil {
+			log.Println(err)
+		}
+		err = json.Unmarshal(bidsJSON.([]byte), &bids)
+		if err != nil {
+			log.Println(err)
+		}
+
+		res = append(res, asks, bids)
+	}
+
+	return res, nil
+}
+
+func (psq *PostgresStorage) InsertOrderBook(exchange_name, pair string, orderBook []*model.DepthOrder) error {
+	query := `INSERT INTO order_book (exchange, pair, asks, bids) VALUES ($1, $2, $3, $4)`
+
+	asksJson, _ := json.Marshal(orderBook[0])
+	bidsJson, _ := json.Marshal(orderBook[1])
+
+	_, err := psq.storage.Conn.Exec(query, exchange_name, pair, asksJson, bidsJson)
+	if err != nil {
+		return fmt.Errorf("error insert order in table: %s", err)
+	}
+
+	return nil
+}
+
+func (psq *PostgresStorage) SelectHistory(client *model.Client) ([]*model.HistoryOrder, error) {
+	var res []*model.HistoryOrder
+
+	query := `SELECT * FROM order_history 
+					WHERE client_name = $1 AND exchange_name = $2 AND label = $3 AND pair = $4`
+	rows, err := psq.storage.Conn.Query(query, client.ClientName, client.ExchangeName, client.Label, client.Pair)
+	if err != nil {
+		return nil, fmt.Errorf("error query: %s", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var history model.HistoryOrder
+
+		err := rows.Scan(&history.ClientName,
+			&history.ExchangeName,
+			&history.Label,
+			&history.Pair,
+			&history.Side,
+			&history.Type,
+			&history.BaseQty,
+			&history.Price,
+			&history.AlgorithmNamePlaced,
+			&history.LowestSellPrc,
+			&history.HighestBuyPrc,
+			&history.CommissionQuoteQty,
+			&history.TimePlaced)
+		if err != nil {
+			log.Printf("error scan row: %s", err)
+		}
+		res = append(res, &history)
+	}
+
+	return res, nil
+}
+
+func (psq *PostgresStorage) InsertHistory(client *model.Client, order *model.HistoryOrder) error {
+	query := `INSERT INTO order_history (client_name, 
+										exchange_name,
+										label, 
+										pair, 
+										side, 
+										type, 
+										base_qty, 
+										price, 
+										algorithm_name_placed, 
+										lowest_sell_prc, 
+										highest_buy_prc, 
+										commission_quote_qty, 
+										time_placed) 
+									VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)`
+
+	_, err := psq.storage.Conn.Exec(query, client.ClientName, client.ExchangeName, client.Label, client.Pair, order.Side, order.Type, order.BaseQty, order.Price, order.AlgorithmNamePlaced, order.LowestSellPrc, order.HighestBuyPrc, order.CommissionQuoteQty, order.TimePlaced)
+	if err != nil {
+		return fmt.Errorf("error insert order in table: %s", err)
+	}
+
+	return nil
+}
