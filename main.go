f157c32136160c56b0929d06dd82551792cf5373
Ilya-SurkoV <lsurkovl@mail.ru>
2024-07-14 11:25:39 +0300

    refact: main

--- main.go
+++ main.go
@@ -10,41 +10,46 @@ import (
 	"github.com/SurkovIlya/statistics-app/internal/orders"
 	"github.com/SurkovIlya/statistics-app/internal/server"
 	"github.com/SurkovIlya/statistics-app/internal/storage/pg"
-	postg "github.com/SurkovIlya/statistics-app/pkg/postgres"
+	st "github.com/SurkovIlya/statistics-app/pkg/postgres"
 )
 
+const serverPort = "8080"
+
 func main() {
-	pgParams := postg.DBParams{
-		Host:     "db",
-		Port:     "5432",
+	pgParams := st.DBParams{
+		Host:     os.Getenv("POSTGRES_HOST"),
+		Port:     os.Getenv("POSTGRES_PORT"),
 		Username: os.Getenv("POSTGRES_USER"),
 		Password: os.Getenv("POSTGRES_PASSWORD"),
 		Database: os.Getenv("POSTGRES_DB"),
 	}
-	storage, err := postg.Initialize(pgParams)
+
+	conn, err := st.Connect(pgParams)
 	if err != nil {
 		panic(err)
 	}
 
-	pgq := pg.New(storage)
+	db := st.New(conn)
 
-	om := orders.New(pgq)
+	pgq := pg.New(db)
 
-	srv := server.New("8080", om)
+	orderManager := orders.New(pgq)
+
+	srv := server.New(serverPort, orderManager)
 
 	go func() {
 		if err := srv.Run(); err != nil {
-			log.Fatalf("error occured while running http server: %s", err.Error())
+			log.Panicf("error occured while running http server: %s", err.Error())
 		}
 	}()
 
-	log.Print("app Started")
+	log.Println("app Started")
 
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
 	<-quit
 
-	log.Print("app Shutting Down")
+	log.Println("app Shutting Down")
 
 	if err := srv.Shutdown(context.Background()); err != nil {
 		log.Panicf("error occured on server shutting down: %s", err.Error())
